57eb07e312828dd70f4cbba74a22190cab9e7487
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-06-27 18:21:25 +0100

    Add initial index page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import path from "path";
+import indexRouter from "./routes/indexRouter.js";
 
 const app = express();
+
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
